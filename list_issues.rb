1f23e1cebb78999e5f1622fd718f42fb22ef1c9d
Mike West <westmd23@gmail.com>
2016-03-28 16:13:32 -0400

    Create private method for api call

--- list_issues.rb
+++ list_issues.rb
@@ -3,18 +3,24 @@ require 'json'
 require 'pry'
 
 class Issue
-
+  ENDPOINT = "https://api.github.com/issues"
   def initialize(user)
     @user = user
   end
 
   def list
-    uri = URI("https://api.github.com/issues")
+    response = query_list
+    JSON.parse(response)
+  end
+
+  private
+
+  def query_list
+    uri = URI(ENDPOINT)
     http = Net::HTTP.new(uri.hostname, uri.port)
     req = Net::HTTP::Get.new(uri)
     req.basic_auth ENV['GITHUB_USER'], ENV['GITHUB_PASSWORD']
     http.use_ssl = true
-    response = http.request(req)
-    JSON.parse(response.body)
+    response = http.request(req).body
   end
 end
